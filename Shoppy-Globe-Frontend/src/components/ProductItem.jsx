88dd7773c34c77f4f21e04e6f805b80791f48403
deepaktulera <deepakstuleraoo3@gmail.com>
2026-06-02 23:29:52 +0530

    Update styling of product item

--- Shoppy-Globe-Frontend/src/components/ProductItem.jsx
+++ Shoppy-Globe-Frontend/src/components/ProductItem.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import {addItem} from "../redux/slices/cartSlice";
+import { addItem } from "../redux/slices/cartSlice";
 
-const ProductItem = ({ id, title , category , price , thumbnail , description}) => {
+const ProductItem = ({
+  id,
+  title,
+  category,
+  price,
+  thumbnail,
+  description,
+}) => {
   const dispatch = useDispatch();
 
   function handleAddItem() {
-    const item = {id , title , category , thumbnail, price , description}
-    
-    dispatch(addItem(item))
+    const item = { id, title, category, thumbnail, price, description };
+
+    dispatch(addItem(item));
   }
-  
 
   return (
-    <div className="shadow-2xl sm:shadow-none hover:shadow-2xl sm:hover:scale-90 sm:hover:shadow-purple-400 sm:transition-all sm:duration-300 shrink-0 rounded-4xl p-3 w-60 md:w-auto box-border">
+    <div className="shadow-2xl md:shadow-none md:hover:scale-90 md:hover:shadow-2xl md:hover:shadow-purple-400 md:transition-all md:duration-300 shrink-0 rounded-4xl p-3 w-60 md:w-auto box-border">
       <Link to={`/product_detail/${id}`}>
         <div>
           <img src={thumbnail} alt={title} className="brightness-90" />
@@ -26,12 +32,12 @@ const ProductItem = ({ id, title , category , price , thumbnail , description}) 
       <div className="flex justify-center gap-4 mt-2">
         <button
           onClick={handleAddItem}
-          className="px-3 py-2 shadow-2xl shadow-red-500 rounded-full active:scale-90"
+          className="px-3 py-2 shadow-2xl shadow-red-500 rounded-full active:scale-80"
         >
           Add
         </button>
 
-        <button className="px-3 py-2 shadow-2xl shadow-red-500 rounded-full active:scale-90">
+        <button className="px-3 py-2 shadow-2xl shadow-red-500 rounded-full active:scale-80">
           Buy
         </button>
       </div>
@@ -39,4 +45,4 @@ const ProductItem = ({ id, title , category , price , thumbnail , description}) 
   );
 };
 
-export default ProductItem
+export default ProductItem;
